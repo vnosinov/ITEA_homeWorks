16e7d07a6fe8428f0710b3357541da6c2ccba6ad
nvladimir2019 <nosinov@gmail.com>
2021-05-10 19:22:31 +0300

    add hw01 декоратор

---
+++ Lesson01/hv01.py
@@ -0,0 +1,20 @@
+import time
+
+
+def time_of_fun(f):
+    def wrapped(*args):
+        start_time = time.time()
+        res = f(*args)
+        print("{:100.98f}".format(time.time() - start_time))
+        return res
+    return wrapped
+
+
+@time_of_fun
+def func(n):
+    if n in (1, 2):
+        return 1
+    return func(n - 1) + func((n - 2))
+
+
+print(func(10))
