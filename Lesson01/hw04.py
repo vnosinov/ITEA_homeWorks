fc7dbaf4ceb364c7b5f571f4ea2018a687c868cd
nvladimir2019 <nosinov@gmail.com>
2021-05-20 19:03:57 +0300

    [upd] lesson01/hw04 new version

--- Lesson01/hw04.py
+++ Lesson01/hw04.py
@@ -6,21 +6,36 @@
 # ранее сумме и после этого завершить программу.
 
 
+# all_sum = 0
+#
+# EXIT = True
+# print('Введите числовой ряд, символ для выхода"#"')
+# while True:
+#     s = input('Числовой ряд: ')
+#
+#
+#     def sum_list(s):
+#         global EXIT
+#         word_list = s.split()
+#         if "#" in word_list:
+#             EXIT = False
+#         num_list = [int(num) for num in filter(lambda num: num.isnumeric(), word_list)]
+#         return sum(num_list)
+#
+#     all_sum += sum_list(s)
+#     print(all_sum)
+
 all_sum = 0
-
-EXIT = True
 print('Введите числовой ряд, символ для выхода"#"')
-while EXIT:
+while True:
     s = input('Числовой ряд: ')
+    word_list = s.split()
+    # if "#" in word_list:
+    #     print('')
 
+    num_list = [int(num) for num in filter(lambda num: num.isnumeric(), word_list)]
 
-    def sum_list(s):
-        global EXIT
-        word_list = s.split()
-        if "#" in word_list:
-            EXIT = False
-        num_list = [int(num) for num in filter(lambda num: num.isnumeric(), word_list)]
-        return sum(num_list)
-
-    all_sum += sum_list(s)
+    all_sum += sum(num_list)
     print(all_sum)
+    if "#" in word_list:
+        break
