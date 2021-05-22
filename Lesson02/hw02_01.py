d7ed8dbc4a37cc04d0770fb30ddfe2e4933ec4a3
nvladimir2019 <nosinov@gmail.com>
2021-05-22 22:13:41 +0300

    [end] lesson02/hw02_01

--- Lesson02/hw02_01.py
+++ Lesson02/hw02_01.py
@@ -33,10 +33,25 @@ class TownCar(Car):
             print(f'Скорость вашего авто превышает 60 км/с, текущая скорость {self.speed}')
 
 
+class WorkCar(Car):
+    def show_speed(self):
+        super().show_speed()
+        if self.speed > 40:
+            print(f'Скорость вашего авто превышает 40 км/с, текущая скорость {self.speed}')
+
+
 class SportCar(Car):
     def show_speed(self):
         super().show_speed()
         print(f'У Вас СпортКар, гони сколько можешь, текущая скорость {self.speed}')
+
+
+class PoliceCar(Car):
+    def police_on_the_road(self):
+
+        if not self.is_police:
+            return
+        print('Полиця на дороге всем принять в право')
 
 
 if __name__ == "__main__":
@@ -49,3 +64,5 @@ if __name__ == "__main__":
     car2 = TownCar('Зеленый', 'Nissan', False)
     car2.go_car(61)
     car2.show_speed()
+    car3 = PoliceCar("Черно-Белый", "Toyota", False)
+    car3.police_on_the_road()
