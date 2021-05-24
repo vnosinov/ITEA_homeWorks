00e9e8b99134c257259de55e8da4fc595c42c28b
nvladimir2019 <nosinov@gmail.com>
2021-05-24 23:03:04 +0300

    [upd] lesson02/hw02_03

--- Lesson02/hw02_03.py
+++ Lesson02/hw02_03.py
@@ -2,6 +2,31 @@
 # Для объектов класса реализовать метод сложения и вычитания матриц, а также умножения, деления матрицы
 # на число и user-friendly вывода матрицы на экран.
 
-class matrix:
-    def __init__(self, row, col):
+
+class Matrix:
+    def __init__(self, nested_list):
+        self.matrix = nested_list
+
+    def view_matrix(self):  # моя функция
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                print(self.matrix[i][j], '  ', end='')
+            print()
+
+    def __str__(self):  # функцию подсмотрел
+        return '\n'.join([''.join(['%d\t' % i for i in row]) for row in self.matrix])
+
+
+    def __add__(self, other):
         pass
+
+
+
+
+if __name__ == '__main__':
+    l1 = [[1, 2], [4, 5], [7, 8]]
+    l2 = [[1, 1], [1, 1], [1, 1]]
+
+    m1 = Matrix(l1)
+    m1.view_matrix()
+    print(m1.__str__())
