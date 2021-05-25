3d591c430a7515ece347dd22028d2dd4c1c55078
nvladimir2019 <nosinov@gmail.com>
2021-05-25 22:54:22 +0300

    [fin] lesson02/hw02_03 финальная версия

--- Lesson02/hw02_03.py
+++ Lesson02/hw02_03.py
@@ -7,26 +7,65 @@ class Matrix:
     def __init__(self, nested_list):
         self.matrix = nested_list
 
-    def view_matrix(self):  # моя функция
-        for i in range(len(self.matrix)):
-            for j in range(len(self.matrix[i])):
-                print(self.matrix[i][j], '  ', end='')
-            print()
-
     def __str__(self):  # функцию подсмотрел
         return '\n'.join([''.join(['%d\t' % i for i in row]) for row in self.matrix])
 
-
     def __add__(self, other):
-        pass
+        result = Matrix(other)
+        result.matrix = []
+        for i in range(len(self.matrix)):
+            temp = []
+            for j in range(len(self.matrix[i])):
+                x = self.matrix[i][j] + other.matrix[i][j]
+                temp.append(x)
+            result.matrix.append(temp)
+        return result
 
+    def __sub__(self, other):
+        result = Matrix(other)
+        result.matrix = []
+        for i in range(len(self.matrix)):
+            temp = []
+            for j in range(len(self.matrix[i])):
+                x = self.matrix[i][j] - other.matrix[i][j]
+                temp.append(x)
+            result.matrix.append(temp)
+        return result
 
+    def __mul__(self, number):
+        result = Matrix(self.matrix)
+        result.matrix = []
+        for i in range(len(self.matrix)):
+            temp = []
+            for j in range(len(self.matrix[i])):
+                x = self.matrix[i][j] * number
+                temp.append(x)
+            result.matrix.append(temp)
+        return result
+
+    def __truediv__(self, number):
+        if number == 0 :
+            return print('Ошибка, деление на ноль')
+        result = Matrix(self.matrix)
+        result.matrix = []
+        for i in range(len(self.matrix)):
+            temp = []
+            for j in range(len(self.matrix[i])):
+                x = self.matrix[i][j] / number
+                temp.append(x)
+            result.matrix.append(temp)
+        return result
 
 
 if __name__ == '__main__':
-    l1 = [[1, 2], [4, 5], [7, 8]]
-    l2 = [[1, 1], [1, 1], [1, 1]]
+    l1 = [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
+    l2 = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 
     m1 = Matrix(l1)
-    m1.view_matrix()
-    print(m1.__str__())
+    m2 = Matrix(l2)
+    print(f'Матрица1 \n{m1}')
+    print(f'Матрица2 \n{m2}')
+    print(f'Сложение \n{m1.__add__(m2)}')
+    print(f'Вычитание \n{m1.__sub__(m2)}')
+    print(f'Умножение \n{m1.__mul__(2)}')
+    print(f'Деление \n{m1.__truediv__(3)}')
