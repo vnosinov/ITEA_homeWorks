fbf3dd92c519d2ee30a4c8c15e12ba61def1647e
nvladimir2019 <nosinov@gmail.com>
2021-05-29 22:25:14 +0300

    [upd] Lesson03/ add test_func "__add__"

--- tests/test_matix.py
+++ tests/test_matix.py
@@ -2,11 +2,34 @@ from Lesson03.hw03_02 import Matrix
 import pytest
 
 
-def test_matrix_add_func():
-    l1 = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    l2 = [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
-    l3 = [[3, 3, 3], [3, 3, 3], [3, 3, 3]]
-    m1 = Matrix(l1)
-    m2 = Matrix(l2)
-    m3 = Matrix(l3)
-    assert m1 + m2 == m3
+@pytest.mark.parametrize("data_matrix1, data_matrix2, exam_res", [
+    (
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]],
+
+            [[1, 1, 1],
+             [1, 1, 1],
+             [1, 1, 1]],
+
+            [[2, 2, 2],
+             [2, 2, 2],
+             [2, 2, 2]],
+    ),
+    (
+            [[2, 2, 2],
+             [-1, -1, -1],
+             [12, 12, 12]],
+
+            [[-1, 1, 1],
+             [1, 0, 1],
+             [1, 1, 7]],
+
+            [[1, 3, 3],
+             [0, -1, 0],
+             [13, 13, 19]],
+    ),
+])
+def test_matrix_add_func(data_matrix1, data_matrix2, exam_res):
+    res_matrix = Matrix(data_matrix1) + Matrix(data_matrix2)
+    assert res_matrix.matrix == Matrix(exam_res).matrix
