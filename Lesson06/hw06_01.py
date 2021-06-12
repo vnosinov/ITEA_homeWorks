767bba863385896e38cc4d4fad12f42b50a5e70d
nvladimir2019 <nosinov@gmail.com>
2021-06-12 19:44:27 +0300

    [upd] Lesson06 inserts реализованы

--- Lesson06/hw06_01.py
+++ Lesson06/hw06_01.py
@@ -28,11 +28,20 @@ def create_table(conn_name, table_name, sql_script):
         print(error)
 
 
-def insert_data_in_table(conn_name, sql_script, data_into):
+def insert_in_department(conn_name, sql_script, data_into):
     try:
         with conn_name, conn_name.cursor() as cursor:
             for data in data_into:
                 cursor.execute(sql_script, (data,))
+    except(Exception, Error) as error:
+        print(error)
+
+
+def insert_in_tables(conn_name, sql_script, data_into):
+    try:
+        with conn_name, conn_name.cursor() as cursor:
+            for data in data_into:
+                cursor.execute(sql_script, data)
     except(Exception, Error) as error:
         print(error)
 
@@ -72,28 +81,51 @@ create_table_orders = sql.SQL("""
 );
 """)
 
-data_departments = ['Тех. поддержка',
-                    'Бухгалтерия',
-                    'Администрация',
-                    'Продажа']
+data_departments = ["Тех. поддержка",  # 1
+                    "Бухгалтерия",     # 2
+                    "Администрация",   # 3
+                    "Продажа"]         # 4
+
+
+data_employees = [
+    ("Иванов Иван Иванович", "Инженер", 1),
+    ("Сидоров Петр Иванович", "Админ", 1),
+    ("Кирилова Мария Ивановка", "Бухгалтер", 2),
+    ("Хараман Тая Федоровна", "Главный бухгалтер", 2),
+    ("Крикунов Давид Янович", "Директор", 3),
+    ("Красоткина Яна Ивановна", "Секретарь", 3),
+    ("Янов Ян Янович", "Менеджер", 4),
+    ("Зайцев Дмитрий Юрьевич", "Продавец", 4),
+    ("Климова Ирина Петровна", "Кассир", 4),
+    ("Крылов Владимир Андреевич", "Старший продавец", 4)]
+
+data_orders = [
+    ('2021-01-01', '2021-01-01', "support", "fsdf", "new", 10001, 3),
+    ('2021-01-02', '2021-01-02', "support", "fsdапапf", "new", 10002, 3),
+    ('2021-01-01', '2021-01-03', "sale", "fsпавпdf", "in progress", 10003, 4),
+    ('2021-03-02', '2021-03-04', "support", "afadffsdf", "done", 10004, 5),
+    ('2021-05-01', '2021-05-10', "sale", "ewrrfsdf", "done", 10005, 5),
+    ('2021-06-01', '2021-06-07', "business", "frffsdf", "done", 10006, 10)]
+
 
 INSERT_QUERY_DEPARTMENTS = sql.SQL("""
     INSERT INTO departments (department_name) VALUES (%s)
 """)
 
+INSERT_QUERY_EMPLOYEES = sql.SQL("""
+    INSERT INTO employees (fio, position, department_id) VALUES (%s, %s, %s)
+""")
+
+INSERT_QUERY_ORDERS = sql.SQL("""
+    INSERT INTO orders (created_dt, updated_dt, type_order, description, status,
+                        serial_number, creator_id) VALUES (%s, %s, %s, %s, %s, %s, %s)
+""")
 
 # create_table(connect_db, "departments", create_table_departments)
 # create_table(connect_db, "employees", create_table_employees)
 # create_table(connect_db, "orders", create_table_orders)
-#
-insert_data_in_table(connect_db, INSERT_QUERY_DEPARTMENTS, data_departments)
+# #
+# insert_data_in_table(connect_db, INSERT_QUERY_DEPARTMENTS, data_departments)
+# insert_in_tables(connect_db, INSERT_QUERY_EMPLOYEES, data_employees)
+insert_in_tables(connect_db, INSERT_QUERY_ORDERS, data_orders)
 
-
-# try:
-#     with connect_db:
-#         with connect_db.cursor() as cursor:
-#             for name in data_departments:
-#                 print(name)
-#                 cursor.execute(INSERT_QUERY_DEPARTMENTS, (name,))
-# except(Exception, Error) as error:
-#     print(error)
