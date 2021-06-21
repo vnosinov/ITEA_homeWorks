3034d59cd35539e596b9c7ea1f3ed40d508766f7
nvladimir2019 <nosinov@gmail.com>
2021-06-21 10:35:26 +0300

    [new] Lesson08 декоратор для класса с записью в текстовый файл

--- Lesson08/hw08_1.py
+++ Lesson08/hw08_1.py
@@ -12,3 +12,29 @@
 json-файл с именем вида <id записи>.json. Если id не существует - выдавать ошибку.
 """
 
+
+from datetime import datetime
+
+
+def deco_class_info(cls):
+    def inner():
+        with open("class_info.txt", "a", encoding="utf-8") as f:
+            info = f"{datetime.now()} Создан экземпляр класса {cls} по адресу памяти {hex(id(cls))}\n"
+            f.write(info)
+
+        return cls
+    return inner
+
+
+@deco_class_info
+class Class1:
+    pass
+
+
+@deco_class_info
+class Class2:
+    pass
+
+
+c1 = Class1()
+c2 = Class1()
